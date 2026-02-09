508123c50059e966812a5a50fa809ba29fd00b13
sathvikmotagi <116015785+sathvikmotagi@users.noreply.github.com>
2026-02-09 07:23:58 +0530

    Add files via upload

---
+++ hyprlandconf/mine.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+cd Downloads
+#!/bin/sh
+#!/bin/sh
+export _JAVA_OPTIONS='-Dsun.java2d.uiScale=120% -Dsun.java2d.dpiaware=true'
+
+export GDK_SCALE=1
+export GDK_DPI_SCALE=1
+
+export GLFW_PLATFORM=x11
+export _JAVA_AWT_WM_NONREPARENTING=1
+/usr/lib/jvm/java-17-openjdk/bin/java -jar TLauncher.jar
+
+# enc
